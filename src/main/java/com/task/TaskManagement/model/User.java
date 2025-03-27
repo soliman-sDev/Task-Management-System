aa39189694fe182673502e1ad30897666198f7fd
soliman-sDev <202615672+soliman-sDev@users.noreply.github.com>
2025-03-27 23:10:39 +0200

    user_Entity

---
+++ src/main/java/com/task/TaskManagement/model/User.java
@@ -0,0 +1,30 @@
+package com.task.TaskManagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+}
